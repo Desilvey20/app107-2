35ba79fddc2fff4fd97b4f17817a5be33b52d6c9
Desilvey20 <101611124+Desilvey20@users.noreply.github.com>
2022-05-16 20:08:29 -0700

    Add files via upload

---
+++ src/services/dataService.jsx
@@ -0,0 +1,29 @@
+var catalog = [
+    {
+        _id: "5f40a6baac77a903d8f682c6",
+        price: 12.32,
+        stock: 13,
+        title: "Chaga",
+        image: "chaga.webp",
+        discount: 5,
+        category: "mushrooms",
+      },
+      {
+        _id: "5f40a6ba41d9e5044ced586c",
+        price: 22.71,
+        stock: 21,
+        title: "Reshi",
+        image: "reshi",
+        discount: 10,
+        category: "mushrooms",
+      },
+      {
+        _id: "5f40a6baa9e6e22fa1d5fac4",
+        price: 32.0,
+        stock: 20,
+        title: "Lion's mane",
+        image: "lions mane",
+        discount: 0,
+        category: "mushrooms",
+      }
+    ]
